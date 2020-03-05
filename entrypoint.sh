a3219a4425fc88cdb8df93decf5824d029f1380c
Patrick Dreker <patrick.dreker@proact.de>
2020-03-05 15:03:41 +0100

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,12 +19,27 @@ done
 
 ls -lR $GITHUB_WORKSPACE
 
+RESULT=0
+
 echo "Deploying pipes from $GITHUB_WORKSPACE/$GITHUB_REPOSITORY/$INPUT_TESTDIR"
 for PIPE in $GITHUB_WORKSPACE/$GITHUB_REPOSITORY/$INPUT_TESTDIR/pipe*.json; do
-    python3 /elasticcheck.py --prepare http://localhost:9200 $PIPE
+    python3 /elasticcheck.py --prepare http://localhost:9200 $PIPE | grep -qv FAILED
+    if [ $? -ne 0 ]; then
+        RESULT=1
+    fi
 done
 
 echo "Running tests from $GITHUB_WORKSPACE/$GITHUB_REPOSITORY/$INPUT_TESTDIR"
 for TEST in $GITHUB_WORKSPACE/$GITHUB_REPOSITORY/$INPUT_TESTDIR/test*.json; do
-    python3 /elasticcheck.py http://localhost:9200 $TEST
+    python3 /elasticcheck.py http://localhost:9200 $TEST | grep -qv FAILED
+    if [ $? -ne 0 ]; then
+        RESULT=2
 done
+
+if [ $RESULT -eq 0 ]; then
+    echo ::set-output name=RESULT::SUCCESS
+elif [ $RESULT -eq 1 ]
+    echo ::set-output name=RESULT::PIPEFAILED
+elif [ $RESULT -eq 2 ]
+    echo ::set-output name=RESULT::FAILED
+fi
