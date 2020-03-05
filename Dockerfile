27b52b24639f3860d260fadd37e509ab517c9d16
Patrick Dreker <patrick.dreker@proact.de>
2020-03-05 14:12:10 +0100

    first test version

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM docker:stable
 
+RUN apk add python3 py3-requests
+COPY elasticcheck/elasticcheck.py /elasticcheck.py
+
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,27 @@ docker_run="docker run -d -p 9200:9200 -p 9300:9300 -e 'discovery.type=single-no
 
 echo "RUNNING: $docker_run"
 sh -c "$docker_run"
+
+
+# Wait 100s for elastic to come up
+echo "WAITING for elasticsearch to become live"
+for i in seq 1 10; do
+    curl -s http://localhost:9200
+    if [ $? -eq 0 ]; then
+        break
+    else
+        sleep 10
+    fi
+done
+
+ls -lR $GITHUB_WORKSPACE
+
+echo "Deploying pipes from $GITHUB_WORKSPACE/$GITHUB_REPOSITORY/$INPUT_TESTDIR"
+for PIPE in $GITHUB_WORKSPACE/$GITHUB_REPOSITORY/$INPUT_TESTDIR/pipe*.json; do
+    python3 /elasticcheck.py --prepare http://localhost:9200 $PIPE
+done
+
+echo "Running tests from $GITHUB_WORKSPACE/$GITHUB_REPOSITORY/$INPUT_TESTDIR"
+for TEST in $GITHUB_WORKSPACE/$GITHUB_REPOSITORY/$INPUT_TESTDIR/test*.json; do
+    python3 /elasticcheck.py http://localhost:9200 $TEST
+done
